24d290f80c31b9de36a53a2f6060002743675964
WRDelmanto <67394462+WRDelmanto@users.noreply.github.com>
2022-10-01 22:26:52 -0300

    [Keyboard] Do not Show Log When Keyboard is Open

--- app/src/main/java/com/wrdelmanto/papps/utils/KeyboardExtensions.kt
+++ app/src/main/java/com/wrdelmanto/papps/utils/KeyboardExtensions.kt
@@ -2,81 +2,50 @@ package com.wrdelmanto.papps.utils
 
 import android.app.Activity
 import android.content.Context
-import android.graphics.Rect
-import android.util.TypedValue
+import android.os.Build
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
-import kotlin.math.roundToInt
 
 /**
  * Hide soft keyboard after a dialog.
  */
 private fun Context.hideKeyboard(view: View) {
+    if (!view.isKeyboardOpen()) return
+
     val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
 
     logD { "hideKeyboard" }
 }
 
-/**
- * Hide soft keyboard after a dialog.
- */
 fun Fragment.hideKeyboard() = view?.let { activity?.hideKeyboard(it) }
 
-/**
- * Hide soft keyboard after a dialog.
- */
 fun Activity.hideKeyboard() = hideKeyboard(currentFocus ?: View(this))
 
 /**
  * Open soft keyboard after a dialog.
  */
+@Suppress("DEPRECATION")
 private fun Context.openKeyboard(view: View) {
+    if (view.isKeyboardOpen()) return
+
     val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0)
 
     logD { "openKeyboard" }
 }
 
-/**
- * Open soft keyboard after a dialog.
- */
 fun Fragment.openKeyboard() = view?.let { activity?.openKeyboard(it) }
 
-/**
- * Open soft keyboard after a dialog.
- */
 fun Activity.openKeyboard() = openKeyboard(currentFocus ?: View(this))
 
 /**
- * Get root view.
+ * Check if keyboard is already open.
  */
-fun Activity.getRootView(): View = findViewById(android.R.id.content)
-
-/**
- * Convert Dp To Px.
- */
-fun Context.convertDpToPx(dp: Float): Float {
-    return TypedValue.applyDimension(
-        TypedValue.COMPLEX_UNIT_DIP,
-        dp,
-        this.resources.displayMetrics
-    )
-}
-
-/**
- * Check if keyboard is opened.
- */
-fun Fragment.isKeyboardOpen(): Boolean? = view?.let { activity?.isKeyboardOpen() }
-
-/**
- * Check if keyboard is opened.
- */
-fun Activity.isKeyboardOpen(): Boolean {
-    val visibleBounds = Rect()
-    this.getRootView().getWindowVisibleDisplayFrame(visibleBounds)
-    val heightDiff = getRootView().height - visibleBounds.height()
-    val marginOfError = this.convertDpToPx(50F).roundToInt()
-    return heightDiff > marginOfError
+fun View.isKeyboardOpen(): Boolean {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        WindowInsetsCompat.toWindowInsetsCompat(rootWindowInsets).isVisible(WindowInsetsCompat.Type.ime())
+    else true
 }
