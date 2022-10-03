7bfe9a9b54ec30d61a0d5b44c9abef8512946474
WRDelmanto <67394462+WRDelmanto@users.noreply.github.com>
2022-10-03 16:58:43 -0300

    [Home] Move to Other Location

---
+++ app/src/main/java/com/wrdelmanto/papps/ui/home/HomeFragment.kt
@@ -0,0 +1,40 @@
+package com.wrdelmanto.papps.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import com.wrdelmanto.papps.R
+import com.wrdelmanto.papps.utils.countApps
+import com.wrdelmanto.papps.utils.countGames
+
+class HomeFragment : Fragment() {
+    private lateinit var homeLogo: ImageView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        homeLogo = view.findViewById(R.id.home_logo)
+
+        initiateListeners()
+    }
+
+    private fun initiateListeners() = homeLogo.setOnClickListener { activateEasterEgg() }
+
+    /**
+     * Function created to test features.
+     */
+    private fun activateEasterEgg() {
+        countApps()
+        countGames()
+    }
+}
