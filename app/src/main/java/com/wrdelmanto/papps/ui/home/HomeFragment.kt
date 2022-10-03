1b3f5ed176423d2afae3452aa5f82b5cc8331f16
WRDelmanto <67394462+WRDelmanto@users.noreply.github.com>
2022-10-03 19:03:43 -0300

    Update HomeFragment.kt

--- app/src/main/java/com/wrdelmanto/papps/ui/home/HomeFragment.kt
+++ app/src/main/java/com/wrdelmanto/papps/ui/home/HomeFragment.kt
@@ -7,8 +7,6 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.wrdelmanto.papps.R
-import com.wrdelmanto.papps.utils.countApps
-import com.wrdelmanto.papps.utils.countGames
 
 class HomeFragment : Fragment() {
     private lateinit var homeLogo: ImageView
@@ -33,8 +31,5 @@ class HomeFragment : Fragment() {
     /**
      * Function created to test features.
      */
-    private fun activateEasterEgg() {
-        countApps()
-        countGames()
-    }
+    private fun activateEasterEgg() {}
 }
