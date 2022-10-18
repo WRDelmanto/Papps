4b29e5dc8f360a251bb40dedf36a6cb6c2142587
WRDelmanto <67394462+WRDelmanto@users.noreply.github.com>
2022-10-17 21:29:06 -0300

    Update Activate Easter Egg

--- app/src/main/java/com/wrdelmanto/papps/ui/home/HomeFragment.kt
+++ app/src/main/java/com/wrdelmanto/papps/ui/home/HomeFragment.kt
@@ -8,9 +8,12 @@ import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.wrdelmanto.papps.R
 import com.wrdelmanto.papps.utils.logD
+import com.wrdelmanto.papps.utils.showNormalToast
 
 class HomeFragment : Fragment() {
     private lateinit var homeLogo: ImageView
+
+    private var clicksEasterEgg = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,6 +36,15 @@ class HomeFragment : Fragment() {
      * Function created to test features.
      */
     private fun activateEasterEgg() {
-        logD { getString(R.string.easter_egg_activated)}
+        clicksEasterEgg++
+        if (clicksEasterEgg >= CLICKS_TO_ACTIVATE_EASTER_EGG) {
+            homeLogo.setOnClickListener(null)
+            context?.let { showNormalToast(it, getString(R.string.easter_egg_activated)) }
+            logD { getString(R.string.easter_egg_activated)}
+        }
+    }
+
+    private companion object {
+        const val CLICKS_TO_ACTIVATE_EASTER_EGG = 10
     }
 }
