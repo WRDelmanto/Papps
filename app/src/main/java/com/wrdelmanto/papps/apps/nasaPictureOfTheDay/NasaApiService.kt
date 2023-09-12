e84fe9f3c60fa769a2f82386f422c6127b4c176c
WRDelmanto <67394462+WRDelmanto@users.noreply.github.com>
2023-09-12 11:14:28 -0300

    [Nasa - Picture of the day] Update nasa service.kt

--- app/src/main/java/com/wrdelmanto/papps/apps/nasaPictureOfTheDay/NasaApiService.kt
+++ app/src/main/java/com/wrdelmanto/papps/apps/nasaPictureOfTheDay/NasaApiService.kt
@@ -1,5 +1,9 @@
 package com.wrdelmanto.papps.apps.nasaPictureOfTheDay
 
+import android.os.Parcelable
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
@@ -10,14 +14,25 @@ import retrofit2.http.Query
 private const val NASA_PICTURE_OF_THE_DAY_BASE_URL = "https://api.nasa.gov/planetary/"
 const val NASA_PICTURE_OF_THE_DAY_API_KEY = "sdV5nVTPFuigmh7gySTWgg4IiLCRl95gjbdzGiGZ"
 
+@Parcelize
+data class NasaPictureOfTheDayData(
+    @SerializedName("copyright") @Expose val copyright: String,
+    @SerializedName("date") @Expose val date: String,
+    @SerializedName("explanation") @Expose val explanation: String,
+    @SerializedName("hdurl") @Expose val hdUrl: String,
+    @SerializedName("media_type") @Expose val mediaType: String,
+    @SerializedName("service_version") @Expose val version: String,
+    @SerializedName("title") @Expose val title: String,
+    @SerializedName("url") @Expose val url: String,
+    @SerializedName("thumbnail_url") @Expose val thumbnailUrl: String
+) : Parcelable
+
 /**
  * Retrofit service object for creating api calls
  */
 interface NasaApiService {
     @GET("apod")
-    suspend fun getNasaPictureOfTheDayData(
-        @Query("api_key") apiKey: String, @Query("thumbs") thumbs: Boolean = true
-    ): NasaPictureOfTheDayData
+    suspend fun getNasaPictureOfTheDayData(@Query("api_key") apiKey: String): NasaPictureOfTheDayData
 }
 
 /**
